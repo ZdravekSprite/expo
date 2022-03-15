251e25b0d0579fe7ab311adcdb32f4b5bd76cc8b
Zdravko Šplajt <zdravek.sprite@gmail.com>
2022-03-15 21:08:22 +0100

    rounded button

---
+++ FocusTime/src/components/RoundedButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+export const RoundedButton = ({
+  style = {},
+  textStyle = {},
+  size = 125,
+  ...props
+}) => {
+  return (
+    <TouchableOpacity style={[styles(size).radius, style]}>
+      <Text style={[styles(size).text, textStyle]}>{props.title}</Text>
+    </TouchableOpacity >
+  )
+};
+
+const styles = (size) => StyleSheet.create({
+  radius: {
+    borderRadius: size / 2,
+    width: size,
+    height: size,
+    alignItems: 'center',
+    borderColor: '#000',
+    borderWidth: 2
+  },
+  text: {
+    color: '#333',
+    fontSize: size / 2
+  },
+})
